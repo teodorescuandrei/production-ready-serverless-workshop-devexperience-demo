0c9a77d45cad91310beeb2528775e679735bf3f6
Andrei Teodorescu <andrei.teodorescu@travelex.com>
2019-04-20 16:33:54 +0300

    push changes

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,6 +1,6 @@
 const APP_ROOT = '../../'
 const _ = require('lodash')
-
+const util = require('util')
 const aws4 = require('aws4')
 const URL = require('url')
 const http = require('superagent-promise')(require('superagent'), Promise)
@@ -76,7 +76,7 @@ const respondFrom = async (httpRes) => {
   }
 
 const viaHandler = async (event, functionName) => {
-    const handler = require(`${APP_ROOT}/functions/${functionName}`).handler
+    const handler = util.promisify(require(`${APP_ROOT}/functions/${functionName}`).handler)
     console.log(`invoking via handler function ${functionName}`)
   
     const context = {}
